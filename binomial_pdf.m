c73a7942e11e02422c6247ae0659fb6d616000a1
Zach Dingels <dingelsz@stat-course-18.netid.washington.edu>
2016-01-13 15:25:11 -0800

    Binomial
    Bad connection so there will be frequent saves.

---
+++ binomial_pdf.m
@@ -0,0 +1,22 @@
+function [ pmf ] = binomial_pdf( input_vector, sample_size, p_success )
+%--------------------------------------------------------------------------
+% FUNCTION: binomial_pdf
+%--------------------------------------------------------------------------
+%
+% DESCRIPTION: Calculate the pmf using a  distribution.
+%
+% INPUT: 
+%   input_vector    Vectr
+%   sample_size     Scalar
+%   mean            Scalar
+%
+% OUTPUT:
+%   pmf             Vector
+%
+%--------------------------------------------------------------------------
+
+%--------------------------------------------------------------------------
+% END OF FUNCTION
+%--------------------------------------------------------------------------
+end
+
